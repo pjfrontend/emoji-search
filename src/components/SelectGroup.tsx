ffda17512948b30b6242f00bde4407cee5fc7d53
wholetone games <135004363+pjfrontend@users.noreply.github.com>
2023-12-11 15:20:08 +0000

    3 css

--- src/components/SelectGroup.tsx
+++ src/components/SelectGroup.tsx
@@ -1,32 +1,22 @@
 import React from 'react';
 import {useNavigate} from 'react-router-dom';
 import {useEmojiData} from '../hooks/useEmojiData';
+import './components.css';
 
 export function SelectGroup() {
   const {groups} = useEmojiData();
   const navigate = useNavigate();
   return (
-    <fieldset
-      style={{
-        display: 'flex',
-        flexFlow: 'wrap',
-      }}
-    >
+    <fieldset className="select-group">
       <legend>Groups</legend>
       {Object.keys(groups).map((g) => (
-        <fieldset
-          key={g}
-          style={{
-            display: 'flex',
-            flexFlow: 'column',
-          }}
-        >
+        <fieldset key={g} className="select-subgroup">
           <legend>{g}</legend>
           {groups[g].map((s) => (
             <button
               key={s}
               type="button"
-              style={{marginBottom: '0.5rem'}}
+              className="subgroup-btn"
               onClick={(e) => {
                 e.preventDefault();
                 navigate(`/group/${s}`);
